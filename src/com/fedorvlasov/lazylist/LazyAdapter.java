03f4d048b68baf20938d67512ef11f7f0c6c19cc
yukihane <yukihane.feather@gmail.com>
2012-05-21 22:54:43 +0900

    fix compile error

--- src/com/fedorvlasov/lazylist/LazyAdapter.java
+++ src/com/fedorvlasov/lazylist/LazyAdapter.java
@@ -20,7 +20,7 @@ public class LazyAdapter extends BaseAdapter {
         activity = a;
         data=d;
         inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        imageLoader=new ImageLoader(activity.getApplicationContext());
+        imageLoader=new ImageLoader(activity.getApplicationContext(), R.drawable.stub);
     }
 
     public int getCount() {
@@ -43,7 +43,7 @@ public class LazyAdapter extends BaseAdapter {
         TextView text=(TextView)vi.findViewById(R.id.text);;
         ImageView image=(ImageView)vi.findViewById(R.id.image);
         text.setText("item "+position);
-        imageLoader.DisplayImage(data[position], image);
+        imageLoader.displayImage(data[position], image);
         return vi;
     }
 }
